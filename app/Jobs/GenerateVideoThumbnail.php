591da64cb0315e96adf604db4d2e65d7771048bb
PedGarBlue <pedro.alejandro.g@hotmail.com>
2024-11-28 10:46:36 -0400

    feat: generate video thumbnail job

---
+++ app/Jobs/GenerateVideoThumbnail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Interfaces\MultimediaService;
+use App\Models\Video;
+use App\Services\FFmpegService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class GenerateVideoThumbnail implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Video $video)
+    {
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(MultimediaService $multimediaService): void
+    {
+        $thumbnail_path = $multimediaService->generateVideoThumbnail($this->video->url);
+
+        $this->video->update([
+            'thumbnail' => $thumbnail_path,
+        ]);
+    }
+}
