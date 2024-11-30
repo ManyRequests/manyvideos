9fec88f3e7ae1847381a1fcee8fdbf11ee3a1e8e
PedGarBlue <pedro.alejandro.g@hotmail.com>
2024-11-29 20:31:45 -0400

    feat(home): render videos with comments count

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,6 +38,7 @@ class HomeController extends Controller
             })
             ->withMinAttributes()
             ->with('user', 'tags')
+            ->withCommentsCount()
             ->latest()
             ->paginate($perpage);
 
