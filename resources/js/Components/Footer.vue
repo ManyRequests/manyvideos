39824278ce9335868e1c4ce267ba092a157053a9
PedGarBlue <pedro.alejandro.g@hotmail.com>
2024-11-30 19:01:45 -0400

    fix: size of "made by" in footer mobile view

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -9,7 +9,7 @@ const page = usePage()
     <footer class=" bg-slate-900 flex flex-wrap">
         <div class="py-8 px-8 w-full flex">
             <ApplicationLogo class="h-10" />
-            <div class="ml-auto my-auto pr-10 text-gray-500 font-medium">
+            <div class="ml-auto my-auto text-xs sm:text-base sm:pr-10 text-gray-500 font-medium">
                 Made by <a href="https://budare.dev" target="_blank" class="text-indigo-400">Pedro García - Budaredev</a>
             </div>
         </div>
